671329f4228f04342f63e13861d76b8b17e9d1a5
Rohit Vempa <rohit@Rohits-MacBook-Pro.local>
2026-07-04 16:40:09 +0530

    added a comment

--- src/main/java/com/example/hospitalmanagement/HospitalmanagementApplication.java
+++ src/main/java/com/example/hospitalmanagement/HospitalmanagementApplication.java
@@ -8,6 +8,7 @@ public class HospitalmanagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HospitalmanagementApplication.class, args);
+		// New project
 	}
 
 }
